adeeae827c11efca9d253c510c28def5a07c4ff4
Gabe <gabe0000@gmail.com>
2020-11-18 19:53:12 -0500

    GO

--- newuser.py
+++ newuser.py
@@ -38,74 +38,88 @@ print("Lez get it...")
 
 #Searches twitter for tweets containing the words input here, couple extra parameters on how it should return the results
 public_tweets = api.search('fuckery, -rt', lang="en",
-                            count=10000, tweet_mode="extended", result_type="recent",)
+                           count=10000, tweet_mode="extended", result_type="recent",)
 
 #def is defining a function, starting with its name.  This function will be called later in the program
+
+
 def usermatch():
     print("")
     print("Executing.............")
+
+    #build a local list of all the people I have reached out to before compiled from from external database
     current_usernames = []
     for x in mycol.find():
         current_usernames.append(x["username"])
-    
+
+    #builds a locl list of people that dont accept DM's compiled from external database
     no_DMs = []
     for x in noDMs.find():
         no_DMs.append(x["username"])
 
+    #loops through the tweets we searched, variable created for usernames
     for tweet in public_tweets:
         tweeter_name = tweet.user.screen_name
+
+        #Now we start deciding what to do with each tweet
         def add_attempt_1():
-            
+
+            #if the person writing the tweet is already on our no DM's list, it gives us a notification
             if tweeter_name in no_DMs:
                 print("We don't fux with " + tweeter_name)
-            else:
 
+            #if we have tried them before, it gives this notification
+            else:
                 if tweeter_name in current_usernames:
                     print("match found")
+
+                #If we havent tried them before, we start trying this stuff
                 else:
+                    #try sending this DM
                     try:
                         api.send_direct_message(
                             tweet.user.id, "Help Fight the Fuckery!  Wanted to see if I can get a follow back?")
+                    #If they won't accept DM's, we put them on the NO DM's list in the external database
                     except:
                         blockedDM = {"username": tweet.user.screen_name,
-                                    "followers": tweet.user.followers_count}
+                                     "followers": tweet.user.followers_count}
                         noDMs.insert_one(blockedDM)
-                        print("We dont fux wit " +tweet.user.screen_name)             
+                        print("We dont fux wit " + tweet.user.screen_name)
+                    #if DM does go through, we add them to the external database of people we have reached and also how many followers they have
                     else:
-                        print(tweeter_name+" has been added to the usernames list!!!")
+                        print(tweeter_name +
+                              " has been added to the usernames list!!!")
                         mydict = {"username": tweet.user.screen_name,
-                                "followers": tweet.user.followers_count}
+                                  "followers": tweet.user.followers_count}
                         mycol.insert_one(mydict)
 
+                        #here we try to favorite their tweet
                         try:
                             api.create_favorite(tweet.id)
+                        #if it doesnt work, error notification
                         except:
                             print("***Could not favorite")
+                        #here we try to add them as a friend
                         try:
                             api.create_friendship(tweet.user.id)
+                        #error message if it doesn't work
                         except:
                             print("***Could not friend")
-                        
-                    
-                                
 
+                #if the tweet has been favorited more than the below defined amount of times, we retweet it
                 if tweet.favorite_count >= 100:
                     try:
                         api.retweet(tweet.id)
+                    #if it doesnt work
                     except:
                         print("***Could Not Retweet")
-        
-        
-      
+
+        #this executes the
         add_attempt_1()
-        
-            
-    #print(current_usernames)
 
 
-
-while 5<10:
+#tells it to run  and then run again every 15 minutes as long as the if condition is true
+while 5 < 10:
     usermatch()
     print("***********************************finished loop***********************************")
     time.sleep(1000)
-       
